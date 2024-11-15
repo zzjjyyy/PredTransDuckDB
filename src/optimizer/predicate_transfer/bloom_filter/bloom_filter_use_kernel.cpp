d1c52f4c7bdf48b79ff342632dadbab470311dd3
Junyi Zhao <junyi@hotel.iiis.io>
2024-11-15 10:05:03 +0800

    Fix the bug that output result is incorrect when directly read from the .db file

--- src/optimizer/predicate_transfer/bloom_filter/bloom_filter_use_kernel.cpp
+++ src/optimizer/predicate_transfer/bloom_filter/bloom_filter_use_kernel.cpp
@@ -19,6 +19,9 @@ void BloomFilterUseKernel::filter(const vector<Vector> &result,
     for(int i = 1; i < bloom_filter->BoundColsApplied.size(); i++) {
 	    VectorOperations::CombineHash(hashes, const_cast<Vector &>(result[bloom_filter->BoundColsApplied[i]]), row_num);
     }
+    if(hashes.GetVectorType() == VectorType::CONSTANT_VECTOR) {
+        hashes.Flatten(row_num);
+    }
     bloom_filter->Find(arrow::internal::CpuInfo::AVX2, row_num, (hash_t*)hashes.GetData(), sel, result_count, false);
 
     approved_tuple_count = result_count;
